4dd08896df033f89d6d3ead004a788820d368cba
Anish M Salve <anishsalve123@gmail.com>
2024-11-25 20:18:20 +0530

    Add files via upload

---
+++ PythonBasics01.py
@@ -0,0 +1,35 @@
+'''print('hello world')'''
+
+# commenting in python
+# a = 5
+
+''' print(id(a)) checking memory in python'''
+
+# python is a case sensitive language
+
+# import keyword
+'''keywords = print(keyword.kwlist)'''
+
+# print(help(True))
+
+# data types
+'''string'''
+# a = 'good morning'
+# print(type(a))
+
+'''list'''
+# l1 = [2,4,6,8]
+# print(type(l1))
+
+'''tuple'''
+# t1 = (2,4,6,8)
+# print(type(t1))
+
+'''set'''
+# s1 ={2,4,6,8}
+# print(type(s1))
+
+'''dictionary'''
+# d1 = {'a':35,'b':37}
+# print(type(d1))
+
